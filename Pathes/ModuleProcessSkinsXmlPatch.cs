b6a2a673c8d53b22546409b367cd45530702984b
Edwin Chen <cnedwin@gmail.com>
2021-06-23 13:32:30 +0800

    update

--- Pathes/ModuleProcessSkinsXmlPatch.cs
+++ Pathes/ModuleProcessSkinsXmlPatch.cs
@@ -10,38 +10,40 @@ using TaleWorlds.ModuleManager;
 using TaleWorlds.MountAndBlade;
 using TaleWorlds.ObjectSystem;
 
-namespace CharacterReload.Patch
+namespace CharacterCreation.Patches
 {
     [HarmonyPatch(typeof(Module), "CreateProcessedSkinsXMLForNative")]
     public static class ModuleProcessSkinsXmlPatch
     {
+
         [HarmonyPrefix]
         public static bool Prefix(ref string __result, ref string baseSkinsXmlPath)
         {
 
-            List<string> elementNameList = new List<string>();
+            List<string> usedPaths = new List<string>();
             List<Tuple<string, string>> toBeMerged = new List<Tuple<string, string>>();
             List<string> xsltList = new List<string>();
+
             List<MbObjectXmlInformation> mbprojXmlList = XmlResource.MbprojXmls.Where(x => x.Id == "soln_skins").ToList();
-            mbprojXmlList.Reverse();
+            mbprojXmlList.Reverse(); 
 
             foreach (MbObjectXmlInformation mbprojXml in mbprojXmlList)
             {
                 if (File.Exists(ModuleHelper.GetXmlPathForNative(mbprojXml.ModuleName, mbprojXml.Name)))
                 {
-                    elementNameList.Add(ModuleHelper.GetXmlPathForNativeWBase(mbprojXml.ModuleName, mbprojXml.Name));
+                    usedPaths.Add(ModuleHelper.GetXmlPathForNativeWBase(mbprojXml.ModuleName, mbprojXml.Name));
                     toBeMerged.Add(Tuple.Create(ModuleHelper.GetXmlPathForNative(mbprojXml.ModuleName, mbprojXml.Name), string.Empty));
                 }
-                string xsltPathForNative = ModuleHelper.GetXsltPathForNative(mbprojXml.ModuleName, mbprojXml.Name);
-
-                xsltList.Add(File.Exists(xsltPathForNative) ? xsltPathForNative : string.Empty);
+                string xslPath = ModuleHelper.GetXsltPathForNative(mbprojXml.ModuleName, mbprojXml.Name);
+                string xsltPath = xslPath + 't';
+                xsltList.Add(File.Exists(xslPath) ? xslPath : (File.Exists(xsltPath) ? xsltPath : string.Empty));
             }
             XmlDocument mergedXmlForNative = MBObjectManager.CreateMergedXmlFile(toBeMerged, xsltList, true);
 
             System.IO.StringWriter stringWriter = new System.IO.StringWriter();
             XmlTextWriter xmlTextWriter = new XmlTextWriter(stringWriter);
             mergedXmlForNative.WriteTo(xmlTextWriter);
-            baseSkinsXmlPath = elementNameList.First();
+            baseSkinsXmlPath = usedPaths.First();
             __result = stringWriter.ToString();
 
             return false;
